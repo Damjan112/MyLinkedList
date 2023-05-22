5fdab0b4879675ac1f0de4d6c8bcfa3091994a9a
Damjan112 <zdravkovskiid@gmail.com>
2023-05-22 13:47:53 +0200

    Add project files.

---
+++ CustomLinkedList/LinkedList.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace CustomLinkedList
+{
+    public class LinkedList<T>
+    {
+        //properties
+        public Node<T> First { get; private set; }
+        public Node<T> Last { get; private set; }
+        public int Count { get; private set; }
+
+        //constructor
+        public LinkedList()
+        {
+            this.First = null;
+            this.Last = null;
+        }
+
+        public void AddFirst(Node<T> newNode)
+        {
+            if(this.First == null)
+            {
+                //this means the linked list is empty.
+                //insert the new node on point the head and tail to the node.
+                this.First = newNode;
+                this.Last = newNode;
+            }
+            else 
+            {
+                newNode.Next = this.First;
+                this.First = newNode;
+            }
+            Count++;
+        }
+
+        public void AddLast(Node<T> newNode)
+        {
+            if (this.First == null)
+            {
+                //this means the linked list is empty.
+                //insert the new node on point the First and Last to the node.
+                this.First = newNode;
+                this.Last = newNode;
+            }
+            else
+            {
+                this.Last.Next = newNode;
+                Last = newNode;
+            }
+            Count++;
+        }
+        public void AddAfter(Node<T> newNode, Node<T> existingNode)
+        {
+            //if you are adding after the last node, then
+            //you need to repoint Last Pointer
+            if(this.Last == existingNode)
+            {
+                Last = newNode;
+            }
+            newNode.Next = existingNode.Next;
+            existingNode.Next = newNode;
+            this.Count++;
+        }
+        public Node<T> Find(T target)
+        {
+            Node<T> currentNode = First;
+            while (currentNode !=null && !currentNode.Data.Equals(target))
+            {
+                currentNode = currentNode.Next;
+            }
+            return currentNode;
+        }
+        public void RemoveFirst()
+        {
+            if(First == null || this.Count == 0)
+            {
+                //nothing to do. Return
+                return;
+            }
+            First = First.Next;
+            this.Count--;
+        }
+        public void Remove(Node<T> doomedNode)
+        {
+            //in a perfect world, you would not need this
+            if(First == null || this.Count == 0)
+            {
+                //nothing to do. Return
+                return;
+            }
+            if(this.First == doomedNode)
+            {
+                this.RemoveFirst();
+                return;
+            }
+
+            //else, you will need to traverse the linked list to find
+            //the doomedNode and remove it
+
+            Node<T> previous = First;
+            Node<T> current = previous.Next;
+            
+            while(current != null && current != doomedNode)
+            {
+                previous = current;
+                current = previous.Next;
+            }
+
+            //remove it
+            if(current != null)
+            {
+                previous.Next = current.Next;
+                this.Count--;
+            }
+        }
+
+        public void Traverse()
+        {
+            Console.WriteLine("\nFirst " + this.First.Data);
+            Console.WriteLine("Last " + this.Last.Data);
+
+            Node<T> node = this.First;
+            while (node.Next != null)
+            {
+                Console.WriteLine(node.Data);
+                node = node.Next;
+            }
+            Console.WriteLine(node.Data);
+        }
+    }
+}
---
+++ MyLinkedList/Program.cs
@@ -0,0 +1,49 @@
+﻿using CustomLinkedList;
+using System;
+
+namespace MyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> ll = new LinkedList<string>();
+            Node<string> a = new Node<string>("aaa");
+            ll.AddFirst(a);
+
+            Node<string> b = new Node<string>("bbb");
+            ll.AddFirst(b);
+
+            Node<string> c = new Node<string>("ccc");
+            ll.AddFirst(c);
+
+            Node<string> d = new Node<string>("ddd");
+            ll.AddFirst(d);
+
+            Node<string> e = new Node<string>("eee");
+            ll.AddLast(e);
+
+            Console.WriteLine("Add after a");
+            ll.AddAfter(new Node<string>("xxx"), a);
+
+            Console.WriteLine("Add after e");
+            ll.AddAfter(new Node<string>("zzz"), e);
+
+            ll.Traverse();
+
+            Node<string> target = ll.Find("ddd");
+            if(target != null)
+            {
+                Console.WriteLine("found: " + target.Data);
+                Console.WriteLine("\nremoving " + target.Data);
+                ll.Remove(target);
+            }
+            else
+            {
+                Console.WriteLine("target not found");
+            }
+            ll.Traverse();
+
+        }
+    }
+}
